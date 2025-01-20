e188e2e1e3588f1b011eaa45e2dfb0c8cec6b427
liu200111 <152293059+liu200111@users.noreply.github.com>
2025-01-20 15:46:27 +0800

    Create 0020_Valid_Parentheses.cpp

---
+++ 0001~0099/0020_Valid_Parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        string brackets_left;
+        
+        for ( char c : s ) {
+            if ( c == '(' || c == '[' || c == '{' )
+                brackets_left += c;
+            else {
+                //避免右括弧多於左括弧的情況
+                if ( brackets_left.length() != 0 ) {
+                    if ( checkisClose( brackets_left[ brackets_left.length() - 1 ] , c ) )
+                        brackets_left.pop_back(); //刪除配對到的左括弧(最後一個)
+                    else return false;
+                }
+                else return false;
+            }
+        }
+        if ( brackets_left.length() > 0 ) return false;
+        else return true;
+    }
+    bool checkisClose(char a, char b) {
+        if ( abs(int(b) - int(a)) <= 2 ) return true;
+        else return false;
+    }
+};
