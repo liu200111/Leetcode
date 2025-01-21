d99073a1815af2af34a7051efc5db5f2c3c3c6ea
liu200111 <152293059+liu200111@users.noreply.github.com>
2025-01-21 16:54:03 +0800

    Update 0029_Divide_Two_Integers.cpp

--- 0001~0099/0029_Divide_Two_Integers.cpp
+++ 0001~0099/0029_Divide_Two_Integers.cpp
@@ -1,32 +1,19 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        int ans = 0;
-        int remainder = dividend;
-      
-        //Handle overflow
-        if (dividend == INT_MIN && divisor == -1) return INT_MAX; 
-        if (dividend == INT_MIN && divisor == 1) return INT_MIN;
-        if (dividend == INT_MIN && divisor == INT_MIN) return 1; 
-        else if (divisor == INT_MIN) return 0;
-      
-        //Time Limit
-        if (dividend == INT_MAX && divisor == -1) return -INT_MAX;
+        long long ans = 0;
+        if (dividend == INT_MIN && divisor == -1) return INT_MAX; // Handle Overflow
+        if (dividend == INT_MAX && divisor == -1) return -INT_MAX; // Handle Time Limit
 
-        //Calculate
-        if (dividend < 0) {
-            while (remainder <= -abs(divisor)) {
-                remainder += abs(divisor);
-                ++ans;
-            }
+        long long divid = labs(dividend);
+        long long divis = labs(divisor);
+        
+        while ( divid >= divis ) {
+            divid -= divis;
+            ++ans;
         }
-        else {
-            while (remainder >= abs(divisor)) {
-                remainder -= abs(divisor);
-                ++ans;
-            }
-        }
-        if ((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0)) return -ans;
+
+        if ( (dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0) ) return -ans;
         else return ans;
     }
 };
