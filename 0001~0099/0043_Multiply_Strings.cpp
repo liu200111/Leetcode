56b06e5883fecf1f380938343d1326a1722d62c4
liu200111 <152293059+liu200111@users.noreply.github.com>
2025-01-23 23:48:45 +0800

    Create 0043_Multiply_Strings.cpp

---
+++ 0001~0099/0043_Multiply_Strings.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        // string -> integer (stoi)
+        // string -> long long (stoll)
+        long long x = std::stoll(num1);
+        long long y = std::stoll(num2);
+        return to_string(x * y);
+    }
+};
