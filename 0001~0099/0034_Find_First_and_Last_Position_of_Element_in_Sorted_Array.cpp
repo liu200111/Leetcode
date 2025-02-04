94a649f155f8698c2313a8cc8b9e8cd1088e25a9
liu200111 <152293059+liu200111@users.noreply.github.com>
2025-02-04 16:17:29 +0800

    Create 0034_Find_First_and_Last_Position_of_Element_in_Sorted_Array.cpp

---
+++ 0001~0099/0034_Find_First_and_Last_Position_of_Element_in_Sorted_Array.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> ans = {-1, -1};
+        // handling empty input
+        if ( nums.size() == 0 ) return ans;
+
+        // get the first position
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] == target) {
+                ans[0] = i;
+                break;
+            }
+            else if (nums[i] > target) return ans;  // If the target is not met
+        }
+
+        // processing target is the last one
+        if (ans[0] == nums.size() - 1) {
+            ans[1] = ans[0];
+            return ans;
+        }
+
+        // get the last position
+        for (int i = ans[0] + 1; i < nums.size(); ++i) {
+            if (nums[i] != target) {
+                ans[1] = i -1;
+                break;
+            }
+            else if ( i == nums.size() - 1) {
+                ans[1] = i;
+                break;
+            }
+        }
+        return ans;
+    }
+};
