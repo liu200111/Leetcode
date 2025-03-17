a9820c2b29266655a1eeae2ff418cf651c513cb8
liu200111 <152293059+liu200111@users.noreply.github.com>
2025-03-17 17:35:25 +0800

    Create 2206_Divide_Array_Into_Equal_Pairs.cpp

---
+++ 2200~2299/2206_Divide_Array_Into_Equal_Pairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        if (nums.size()%2 != 0) return false;
+
+        sort(nums.begin(), nums.end());
+        int i = 0;
+        while(i < nums.size()) {
+            if (nums[i] != nums[i+1]) return false;
+            i += 2;
+        }
+        return true;
+    }
+};
